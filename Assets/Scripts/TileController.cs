52f9e23fdd5c9aa707a7d9632b8edb6acd00c6d6
Mike Marvin <Mike.Marvin@hyland.com>
2021-05-26 15:06:02 -0400

    Draw tiles when moles > 0.

---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+    public const float GAS_CONSTANT = 8.206e-5f; //m^3*atm/K*mol
+
+    public int Id { get; set; } = 0;
+    public float Temperature { get; set; } = 20;
+    public float MoleCount { get; set; } = 0;
+    public bool IsSolid { get; set; } = false;
+
+    public float TemperatureK => Temperature + 273.15f;
+
+    void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+
+    /// <summary>
+    /// Returns the pressure in atm for the given temperature/moles
+    /// </summary>
+    /// <returns>Pressure in atm</returns>
+    public float GetPressure()
+    {
+        if (IsSolid) return 0;
+        return MoleCount * TemperatureK * GAS_CONSTANT;
+    }
+
+    /// <summary>
+    /// Update the MoleCount to fix the pressure
+    /// </summary>
+    /// <param name="pressure">Desired pressure in atm</param>
+    public void SetPressure(float pressure)
+    {
+        if (IsSolid) return;
+        if (pressure <= 0)
+        {
+            MoleCount = 0;
+        }
+        else
+        {
+            MoleCount = pressure / (GAS_CONSTANT * TemperatureK);
+        }
+    }
+
+}
+
--- Assets/Scripts/WorldController.cs
+++ Assets/Scripts/WorldController.cs
@@ -6,12 +6,24 @@ public class WorldController : MonoBehaviour
     public int tiles_height = 48;
     public float diff = 250;
     public float dt = 0.0167f;
+    public GameObject TileSource;
 
+
+    GameObject[] tiles;
     float[] current;
     // Start is called before the first frame update
     void Start()
     {
         current = new float[tiles_width * tiles_height];
+        tiles = new GameObject[tiles_width * tiles_height];
+        for (int i=0; i<tiles_width*tiles_height; i++)
+        {
+            var obj = Instantiate(TileSource);
+            Vector2 pos = indexToXY(i);
+            obj.transform.position = pos;
+            obj.SetActive(true);
+            tiles[i] = obj;
+        }
     }
 
     private void Update()
@@ -19,7 +31,7 @@ public class WorldController : MonoBehaviour
         bool leftClick = Input.GetMouseButton(0);
         if (leftClick)
         {
-            current[index_of(tiles_width / 2, tiles_height / 2)] += 42f;
+            current[indexOf(tiles_width / 2, tiles_height / 2)] += 42f;
         }
     }
 
@@ -29,27 +41,48 @@ public class WorldController : MonoBehaviour
         float[] next = new float[tiles_width * tiles_height];
         diffuse(next, current, diff, dt);
         current = next;
-        //for (int x = 0; x < tiles_width; x++)
-        //{
-        //    for (int y = 0; y < tiles_height; y++)
-        //    {
-        //        float m = current[index_of(x, y)];
-        //        if (m > 0)
-        //        {
-        //            Debug.LogWarning($"({x}, {y}): {m}");
-        //        }
-        //    }
-        //}
+        for (int x = 0; x < tiles_width; x++)
+        {
+            for (int y = 0; y < tiles_height; y++)
+            {
+                int index = indexOf(x, y);
+                float m = current[index];
+                if (tiles[index] != null)
+                {
+                    tiles[index].GetComponent<TileController>().MoleCount = m;
+                    if (m == 0)
+                    {
+                        tiles[index].SetActive(false);
+                    }
+                    else if (!tiles[index].activeSelf)
+                    {
+                        tiles[index].SetActive(true);
+                    }
+                }
+
+                //if (m > 0)
+                //{
+                //    Debug.LogWarning($"({x}, {y}): {m}");
+                //}
+            }
+        }
     }
 
-    int index_of(int x, int y)
+    int indexOf(int x, int y)
     {
         return x + y * tiles_width;
     }
 
+    Vector2 indexToXY(int index)
+    {
+        int x = index % tiles_width;
+        int y = index / tiles_width;
+        return new Vector2(x, y);
+    }
+
     void diffuse(float[] new_state, float[] initial_state, float diff, float dt)
     {
-        //System.Diagnostics.Stopwatch t_start = System.Diagnostics.Stopwatch.StartNew();
+        System.Diagnostics.Stopwatch t_start = System.Diagnostics.Stopwatch.StartNew();
         float a = dt * diff;
         for (int k=0; k<20; k++)
         {
@@ -62,41 +95,41 @@ public class WorldController : MonoBehaviour
                     if (x-1>0)
                     {
                         neighbors++;
-                        calc += new_state[index_of(x - 1, y)];
+                        calc += new_state[indexOf(x - 1, y)];
                     }
                     if (x + 1 < tiles_width)
                     {
                         neighbors++;
-                        calc += new_state[index_of(x + 1, y)];
+                        calc += new_state[indexOf(x + 1, y)];
                     }
                     if (y - 1 > 0)
                     {
                         neighbors++;
-                        calc += new_state[index_of(x, y - 1)];
+                        calc += new_state[indexOf(x, y - 1)];
                     }
                     if (y + 1 < tiles_height)
                     {
                         neighbors++;
-                        calc += new_state[index_of(x, y + 1)];
+                        calc += new_state[indexOf(x, y + 1)];
                     }
                     if (neighbors == 0)
                     {
-                        new_state[index_of(x, y)] = initial_state[index_of(x, y)];
+                        new_state[indexOf(x, y)] = initial_state[indexOf(x, y)];
                         continue;
                     }
                     calc *= a;
-                    calc = (initial_state[index_of(x, y)] + calc) / (1 + neighbors * a);
-                    if (calc < 0.1)
+                    calc = (initial_state[indexOf(x, y)] + calc) / (1 + neighbors * a);
+                    if (calc < 0.01)
                     {
                         calc = 0;
                     }
-                    new_state[index_of(x, y)] = calc;
+                    new_state[indexOf(x, y)] = calc;
 
                     //new_state[index_of(x, y)] = (initial_state[index_of(x, y)] + a * (new_state[index_of(x - 1, y)] + new_state[index_of(x + 1, y)] + new_state[index_of(x, y - 1)] + new_state[index_of(x, y + 1)])) / (1 + 4 * a);
                 }
             }
         }
-        //long t_calc = t_start.ElapsedMilliseconds;
-        //Debug.Log($"Calc: {t_calc} ms");
+        long t_calc = t_start.ElapsedMilliseconds;
+        Debug.Log($"Calc: {t_calc} ms");
     }
 }
